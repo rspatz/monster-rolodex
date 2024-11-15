45c8ad59c4ae7753864737d62c8bb18e24ac1f3b
Robert <rspatz@live.com>
2024-11-15 08:13:51 -0600

    Add renders and re-rendering

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ class App extends Component {
     this.state = {
       monsters: []
     };
+    console.log('constructor');
   }
 
   componentDidMount() {
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users =>
@@ -26,8 +28,17 @@ class App extends Component {
   }
 
   render() {
+    console.log('render');
     return (
       <div className='App'>
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={event => {
+            console.log(event.target.value);
+          }}
+        />
         {this.state.monsters.map(monster => {
           return (
             <div key={monster.id}>
